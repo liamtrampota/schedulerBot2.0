e3b605c7109c7e32a02f65f43131d91d0edcbf17
liamtrampota <ldtrampota@gmail.com>
2018-08-17 11:47:34 -0700

    authorize invited users and button functionality for invitation response

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
 import {User} from './models.js';
 const fetch = require('node-fetch-polyfill')
+
+
+
+
+
+
 //connect to mongoose
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -12,8 +18,8 @@ mongoose.connect(process.env.MONGODB_URI);
 const dialogflow = require('dialogflow');
 
 //calender API function
-function  makeCalendarAPICall(token, conversationId, type, event, user) {
- console.log('makeCalender called with token:', token)
+function  makeCalendarAPICall(user, conversationId, type, event) {
+ console.log("CALENDER API CALLED", )
 
  //sets OAuth2
  const oauth2Client = new google.auth.OAuth2(
@@ -21,7 +27,7 @@ function  makeCalendarAPICall(token, conversationId, type, event, user) {
      process.env.CLIENT_SECRET,
      process.env.REDIRECT_URL
   )
-  oauth2Client.setCredentials(token)
+  oauth2Client.setCredentials(user.token)
 
   //listens for refresh tokens.. not sure how to use exactly
   oauth2Client.on('tokens', (tokens) => {
@@ -45,34 +51,125 @@ function  makeCalendarAPICall(token, conversationId, type, event, user) {
     var startDateTimeNum = Number(startDateTime)
     var endDateTimeNum = startDateTimeNum + (1000*60*30)
     var endDateTime = new Date(endDateTimeNum)
-    User.find({name: {$in: [event.attendees]}}, function(err, users){
-      if(err){
-        console.log(err)
-      } else if(users.length>0){
-        var attendeeArray=users.map((user)=>({displayName:user.name, email:user.email}))
-        calendar.events.insert({
-            calendarId: 'primary', // Go to setting on your calendar to get Id
-            'resource': {
-              'attendees':attendeeArray,
-              'summary': event.subject,
-              'end': {
-                'dateTime': endDateTime,
-                //'timeZone':
-              },
-              'start': {
-                'dateTime':startDateTime,
-                //'timeZone':
-              }
-            }
 
-          }, (err, data) => {
-            if (err) return console.log('The API returned an error: ' + err);
-            else {
-              //console.log('successfully added meeting,', data)
-              rtm.sendMessage('Successfully scheduled the meeting!', conversationId)
-            }
-          })
+    if(event.attendees.length>0){
+
+      User.find({user: {$in: [event.attendees]}}, function(err, users){
+
+        if(err){
+          console.log(err)
+        }
+
+        else if(users.length>0){
+          console.log('FOUND ATTENDEES:', users)
+          var attendeeArray=users.map((user)=>({displayName:user.name, email:user.email}))
+
+          calendar.events.insert({
+              calendarId: 'primary', // Go to setting on your calendar to get Id
+              'resource': {
+                'attendees':attendeeArray,
+                'summary': event.subject,
+                'end': {
+                  'dateTime': endDateTime,
+                  //'timeZone':
+                },
+                'start': {
+                  'dateTime':startDateTime,
+                  //'timeZone':
+                }
+              }
+
+            }, (err, data) => {
+              if (err) return console.log('The API returned an error: ' + err);
+              else {
+                //console.log('successfully added meeting,', data)
+                rtm.sendMessage('Successfully scheduled the meeting!', conversationId)
+                console.log('CALENDAR MEETING INSERTED')
+                event.attendees.forEach(function(id){
+                  web.conversations.open({
+                    token: process.env.SLACK_TOKEN,
+                    users: id,
+                    return_im: false
+                  }).then((response)=>{
+                    web.chat.postMessage({
+                      channel: response.channel.id,
+                      // as_user: true,
+                      "text": "Liam Trampota has invited you to a meeting to discuss life at Thursday 4pm?",
+                      "attachments": [
+                          {
+                              "text": "Can you attend?",
+                              "fallback": "You are unable to choose a game",
+                              "callback_id": "wopr_game",
+                              "color": "#3AA3E3",
+                              "attachment_type": "default",
+                              "actions": [
+                                  {
+                                      "name": "yes",
+                                      "text": "Yes",
+                                      "type": "button",
+                                      "value": "yes"
+                                  },
+                                  {
+                                      "name": "no",
+                                      "text": "No",
+                  					          "style": "danger",
+                                      "type": "button",
+                                      "value": "no"
+                                  },
+                                  {
+                                      "name": "reschedule",
+                                      "text": "Reschedule",
+                                      "type": "button",
+                                      "value": "reschedule",
+                                      "confirm": {
+                                          "title": "Are you sure?",
+                                          "text": "Are you sure?",
+                                          "ok_text": "Yes",
+                                          "dismiss_text": "No"
+                                      }
+                                  }
+                              ]
+                          }
+                      ]
+
+                    })
+                      .then((res) => {
+                        // `res` contains information about the posted message
+                        console.log('Message sent: ', res.ts)
+                      })
+                      .catch(console.error)
+                    })
+
+
+
+                    //
+                    // rtm.sendMessage(user.name +' scheduled a meeting with you at ' + startDateTime, response.channel.id)})
+                })
+              }
+            })
+          }
+
+          else{
+            console.log('ATTENDEES NOT AUTHORIZED')
+            rtm.sendMessage('Successfully added the meeting to your calendar! We will notify the invitees about the meeting', conversationId)
+            event.attendees.forEach(function(id){
+              const url = oauth2Client.generateAuthUrl({
+                access_type: 'offline',
+                approval_prompt: 'force',
+                state: id,
+                scope: scopes
+              })
+              web.conversations.open({
+                token: process.env.SLACK_TOKEN,
+                users: id,
+                return_im: false
+              }).then((response)=>{rtm.sendMessage(user.name + ' invited you to a meeting at ' + startDateTime + '. To add it to your calender or decline the event through Scheduler 2.0, please grant me access to your google calendar by clicking on the link: ' + url, response.channel.id)})
+            })
+          }
+
+        })
       } else {
+        console.log('NO ATTENDEES:', users)
         calendar.events.insert({
             calendarId: 'primary', // Go to setting on your calendar to get Id
             'resource': {
@@ -95,9 +192,7 @@ function  makeCalendarAPICall(token, conversationId, type, event, user) {
             }
           })
       }
-    })
-
-  }
+    }
 
   if(type=='reminder'){
     var correctDate = event.date.slice(0,10)
@@ -119,8 +214,34 @@ function  makeCalendarAPICall(token, conversationId, type, event, user) {
           var a=new Date(data.data.start.date);
           console.log("a :",a)
           var b=Number(a)
-          console.log("b :",b)
-        web.reminder.add
+          console.log('b:', b)
+          var num1 = b-(1000*60*60*7)
+          var num2= b+(1000*60*60*9)
+          console.log("num1:", num1, new Date(num1))
+          console.log("num2:", num2, new Date(num2))
+          web.reminders.add({
+            text:data.data.summary,
+            time:num1,
+            user:user.user,
+            token:process.env.SLACK_USER_TOKEN
+          })
+          .then((res) => {
+            console.log(res)
+          })
+          .catch(console.error)
+
+          web.reminders.add({
+            text:data.data.summary,
+            time:num2,
+            user:user.user,
+            token:process.env.SLACK_USER_TOKEN
+          })
+          .then((res) => {
+            console.log(res)
+          })
+          .catch(console.error)
+
+
           rtm.sendMessage(('successfully scheduled a reminder to '+data.data.summary +' on ' + data.data.start.date), conversationId)}
       }
     )
@@ -184,16 +305,17 @@ rtm.start()
 rtm.on('message', function (event) {
   const conversationId = event.channel;
   console.log('event:', event)
-  const url = oauth2Client.generateAuthUrl({
-    access_type: 'offline',
-    state: event.user,
-    scope: scopes
-  })
+  // const url = oauth2Client.generateAuthUrl({
+  //   access_type: 'offline',
+  //   approval_prompt: 'force',
+  //   state: event.user,
+  //   scope: scopes
+  // })
   User.find({user:event.user}, function(err, user){
     console.log('user:', user)
     if(user.length == 0){
       if(!event.bot_id){
-      rtm.sendMessage(url, conversationId)
+      rtm.sendMessage('http://localhost:5000/authorize?user=' + event.user, conversationId)
     }
     } else{
       console.log('user exists:', user[0])
@@ -231,15 +353,15 @@ rtm.on('message', function (event) {
             //checking which intent was matched - makes corresponding calendar api call
             if(result.intent){
               if(result.intent.displayName == 'reminders.add'){
-                  makeCalendarAPICall(user[0].token, conversationId, 'reminder', {subject:result.parameters.fields.name.stringValue, date: result.parameters.fields['date-time'].stringValue}, event.user)
+                  makeCalendarAPICall(user[0], conversationId, 'reminder', {subject:result.parameters.fields.name.stringValue, date: result.parameters.fields['date-time'].stringValue}, )
               }
               else if(result.intent.displayName == 'Meeting'){
-                var attendees = result.parameters.fields['given-name'].listValue.values.map(function(item){
+                var attendees = result.parameters.fields.any.listValue.values.map(function(item){
                   return item.stringValue
                 })
                 console.log('attendees:', attendees)
                 var dateTime = result.parameters.fields.date.stringValue.slice(0,10)+'T'+result.parameters.fields.time.stringValue.slice(11,19);
-                makeCalendarAPICall(user[0].token, conversationId, 'scheduler', {subject:result.parameters.fields.subject.stringValue, dateTime:dateTime, attendees:attendees}, event.user)
+                makeCalendarAPICall(user[0], conversationId, 'scheduler', {subject:result.parameters.fields.subject.stringValue, dateTime:dateTime, attendees:attendees}, )
               } else {
                 rtm.sendMessage(result.fulfillmentText, conversationId)
               }
@@ -268,7 +390,7 @@ app.get('/oauthcallback', (req, res) => {
   console.log('req:', req)
   console.log('req.body:', req.body)
   oauth2Client.getToken(req.query.code, function (err, token) {
-    console.log('req.query:', req.query)
+    console.log('token:', token)
     if (err) return console.error(err.message)
     else {
       fetch('https://www.googleapis.com/oauth2/v1/userinfo?access_token='+token.access_token)
@@ -292,6 +414,21 @@ app.get('/oauthcallback', (req, res) => {
     console.log('req.query:',req.query)
     res.send('ok')
   })
+})
+
+app.get('/authorize', (req, res) => {
+  console.log('user:', req.query.user)
+  const url = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    approval_prompt: 'force',
+    state: req.query.user,
+    scope: scopes
+  })
+  res.redirect(url)
+})
+
+app.get('/button', (req, res) => {
+  console.log(req.body)
 })
 
 console.log('listening on port 5000')
